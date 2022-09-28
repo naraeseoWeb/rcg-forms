ed15425ed9c99b870a5872300c549bf12dda7928
naraeseo <naraeseoez@gmail.com>
2022-09-28 11:58:17 +0900

    Section 16 210: 입력된 내용이 없을 때 유효성 검증 피드백

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,33 +3,47 @@ import { useState, useRef } from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (e) => {
-    // 1. useState: 입력하는 키마다 유효성 검증
     setEnteredName(e.target.value);
   };
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
     console.log(enteredName, 'enteredName');
 
-    // 2. useRef: 제출 시 한 번에 유효성 검증
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue, 'enteredValue');
-    setEnteredName(''); // 새로 불러왔을 때 빈 값으로 초기값 설정
+    setEnteredName('');
   };
+
+  const nameInputClasses = enteredNameIsValid
+    ? 'form-control'
+    : 'form-control invalid';
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
           ref={nameInputRef}
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
-          value={enteredName} // 새로 불러왔을 때 빈 값으로 초기값 설정
+          value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className='error-text '>Name must not be empty.</p>
+        )}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
